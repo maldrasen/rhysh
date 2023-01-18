6cff95f963ff9df6cffa2c00b87505b41c0f2615
Maldrasen <maldrasen@gmail.com>
2023-01-17 18:48:36 -0600

    Add dungeon builder spec

---
+++ modules/dungeonBuilder/spec/dungeon-builder-spec.js
@@ -0,0 +1,9 @@
+describe('DungeonBuilder', function() {
+
+  it("parses layer names", function() {
+    let info = DungeonBuilder.parseLayerName("Extra 2");
+    expect(info.type).to.equal("extra");
+    expect(info.index).to.equal(1);
+  });
+
+});
