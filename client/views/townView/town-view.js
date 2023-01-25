c8333f8caec05c7952b0108a2dc46a3ddb23368b
Maldrasen <maldrasen@gmail.com>
2023-01-25 10:19:03 -0600

    Add town views

---
+++ client/views/townView/town-view.js
@@ -0,0 +1,26 @@
+window.TownView = (function() {
+
+  function showBlacksmith() {
+
+  }
+
+  function showGuild() {
+
+  }
+
+  function showStore() {
+
+  }
+
+  function showTavern() {
+
+  }
+
+  return {
+    showBlacksmith,
+    showGuild,
+    showStore,
+    showTavern,
+  }
+
+})()
