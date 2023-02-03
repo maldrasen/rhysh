51c9b783f57647b213c9c4d651d9034130eeb89c
Maldrasen <maldrasen@gmail.com>
2023-02-02 21:12:14 -0600

    Working on the select species step

--- client/views/mainMenu/new-game.js
+++ client/views/mainMenu/new-game.js
@@ -1,9 +1,32 @@
 window.NewGame = (function() {
 
+  const SexMapping = {
+    knight:     ['male'],
+    slaver:     ['male','female','futa'],
+    cultist:    ['male','female','futa'],
+    mindbender: ['male','female','futa'],
+    dominatrix: ['female','futa'],
+    chosen:     ['male','futa'],
+  };
+
+  const SpeciesMapping = {
+    knight:     ['elf','orc','minotaur','dragonkind','lupin','satyr'],
+    slaver:     ['elf','orc','lupin','satyr'],
+    cultist:    ['elf','nymph','dragonkind','satyr'],
+    mindbender: ['elf','nymph','dragonkind','satyr'],
+    dominatrix: ['elf','nymph','orc','dragonkind','lupin','satyr'],
+    chosen:     ['elf','orc','minotaur','dragonkind','lupin','satyr'],
+  };
+
   let selectedArchitype = 'knight';
+  let selectedSex = 'male';
+  let selectedSpecies = 'elf';
+
 
   function init() {
-    X.onClick('#newGame .architypes .button', e => { selectArchitype(e.target) });
+    X.onClick('#newGame .architypes .button', e => { selectArchitype(e.target); });
+    X.onClick('#newGame .choose-architype-button', e => { showSpeciesStep(); });
+    X.onClick('#newGame .back-to-architype-button', e => { showArchitypeStep(); });
   }
 
   function show() {
@@ -21,11 +44,63 @@ window.NewGame = (function() {
     X.addClass(target,'selected');
     X.removeClass(`#newGame .descriptions .${name}`,'hide');
 
+    let allowedSexes = SexMapping[name];
+    let allowedSpecies = SpeciesMapping[name];
+
     selectedArchitype = name;
+    selectedSex = allowedSexes[0];
+    selectedSpecies = allowedSpecies[0];
   }
 
+  function showArchitypeStep()  {
+    X.addClass('.species-step','hide');
+    X.addClass('.back-to-architype-button','hide');
+    X.removeClass('.architype-step','hide');
+  }
 
+  function showSpeciesStep() {
+    X.addClass('.architype-step','hide');
+    X.removeClass('.species-step','hide');
+    X.removeClass('.back-to-architype-button','hide');
 
+    X.each('.species-step .sexes a.button', button => {
+      X.removeClass(button,'hide');
+    });
+
+    X.each('.species-step .species a.button', button => {
+      X.removeClass(button,'hide');
+    });
+
+    X.each('.species-step .sexes a.button', button => {
+      let name = X.classesExcept(button, ['button','selected'])[0];
+      if (false == ArrayHelper.contains(SexMapping[selectedArchitype], name)) {
+        X.addClass(button,'hide');
+      }
+    });
+
+    X.each('.species-step .species a.button', button => {
+      let name = X.classesExcept(button, ['button','selected'])[0];
+      if (false == ArrayHelper.contains(SpeciesMapping[selectedArchitype], name)) {
+        X.addClass(button,'hide');
+      }
+    });
+
+    X.addClass(`.species-step .sexes a.${selectedSex}`,'selected');
+    X.addClass(`.species-step .species a.${selectedSpecies}`,'selected');
+
+    updateSpeciesImages();
+  }
+
+  function updateSpeciesImages() {
+    X.each('.species-step .species a', button => {
+      X.removeClass(button, 'male');
+      X.removeClass(button, 'female');
+    });
+
+    X.each('.species-step .species a', button => {
+      X.addClass(button, selectedSex);
+    });
+  }
 
 
 
